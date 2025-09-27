566fadedce9a4aa17dd4d7d179cfc7ab0c0ef643
simonnjoroge <simonnjoroge806@gmail.com>
2025-09-27 19:43:06 +0300

    added logic to filter out booked slots

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -60,15 +60,21 @@ const Appointment = () => {
         let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: true});
 
         let day = currentDate.getDate();
-        let month = date.getMonth() + 1;
-        let year = date.getFullYear();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
 
-      const slotDate = day + "_" + month + "_" + year;
+        const slotDate = day + "_" + month + "_" + year;
+        const slotTime = formattedTime;
+
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true;
         
-        timeSlots.push({
-          dateTime: new Date(currentDate),
-          time: formattedTime
-        })
+        if (isSlotAvailable) {
+          timeSlots.push({
+            dateTime: new Date(currentDate),
+            time: formattedTime
+          })
+        }
+
         //increment current time by 30mins
         currentDate.setMinutes(currentDate.getMinutes() + 30);
       }
@@ -112,7 +118,7 @@ const Appointment = () => {
     }
 
     } catch (error) {
-      toast.error(error.message + "fege")
+      toast.error(error.message)
     }
 
   }
